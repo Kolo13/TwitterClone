17daddec549fec1e8b0580012a09691bb63a1d25
kolo13 <kori.kolodziejczak@gmail.com>
2014-10-06 15:42:56 -0700

    Leo's help you yeah

---
+++ HomeTimelineViewController.swift
@@ -0,0 +1,73 @@
+//
+//  HomeTimelineViewController.swift
+//  TwitterClone
+//
+//  Created by Kori Kolodziejczak on 10/6/14.
+//  Copyright (c) 2014 Kori Kolodziejczak. All rights reserved.
+//
+
+import UIKit
+
+class HomeTimelineViewController: UIViewController, UITableViewDataSource {
+
+  
+  var tweets : [Tweet]?
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        if let path = NSBundle.mainBundle().pathForResource("tweet", ofType: "json") {
+       
+          var error : NSError?
+          let jsonData = NSData(contentsOfFile: path)
+        
+          self.tweets = Tweet.parseJSONDataIntoTweets(jsonData!)
+          self.tweets = self.tweets?.sorted({ (S1:Tweet , S2:Tweet) -> Bool in
+            return S1.text < S2.text
+          })
+         
+      }
+
+        // Do any additional setup after loading the view.
+    }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if self.tweets != nil {
+      return self.tweets!.count
+    }else{
+      return 0
+      
+    }
+  }
+  
+  
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Tweet_Cell", forIndexPath: indexPath) as UITableViewCell
+    let tweet = self.tweets![indexPath.row]
+    cell.textLabel?.text = tweet.text
+    return cell
+    
+  
+  }
+  
+  
+  
+     override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TwitterClone/Tweet.swift
@@ -0,0 +1,41 @@
+//
+//  tweet.swift
+//  TwitterClone
+//
+//  Created by Kori Kolodziejczak on 10/6/14.
+//  Copyright (c) 2014 Kori Kolodziejczak. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+  
+  var text : String
+  
+  init (tweetInfo : NSDictionary) {
+    self.text = tweetInfo["text"] as String
+  }
+  
+  
+  class func parseJSONDataIntoTweets(rawJSONData : NSData) -> [Tweet]? {
+    var error : NSError?
+    if let JSONArray = NSJSONSerialization.JSONObjectWithData(rawJSONData, options: nil, error: &error) as? NSArray {
+    
+      var tweets = [Tweet]()
+      
+      for JSONDictionary in JSONArray {
+        if let tweetDictionary = JSONDictionary as? NSDictionary {
+          var newTweet = Tweet(tweetInfo: tweetDictionary)
+          
+          tweets.append(newTweet)
+        }
+      }
+    return tweets
+      
+    }
+    return nil
+  }
+  
+
+}
+
